7ec25e2439015b1a51d230a7b579e57076db917a
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-01-16 08:12:54 -0700

    added a test comment

--- token/token.sol
+++ token/token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
+// This is a test token.
 contract Token is ERC20, Ownable {
     constructor() ERC20("USDC-Token", "$USDC") {
         _mint(msg.sender, 10000 );
