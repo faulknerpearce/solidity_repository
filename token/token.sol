cb9baf4e483c6f3f2292f10f92d08d91b6dee4db
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-06-18 11:20:36 -0600

    created a token contract

--- token/token.sol
+++ token/token.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract Token is ERC20, Ownable {
-    constructor() ERC20("USDC-Token", "$USDC") {
-        _mint(msg.sender, 10000 );
+    constructor() ERC20("USDC-Token", "$USDC") Ownable(msg.sender) {
+        // Adjust the minting amount considering decimals
+        _mint(msg.sender, 10000);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
