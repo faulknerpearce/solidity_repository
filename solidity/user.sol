8729f2cae3f93c7b14f3f5e9c184b631c2a70f37
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-08 09:27:29 -0700

    updated comments for the user script

--- solidity/user.sol
+++ solidity/user.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+// A simple user management and transfer contract allowing users to create accounts and transfer tokens.
 contract Contract {
 
+    // Defines a structure to store user details.
     struct User {
         uint balance;
         bool isActive;
@@ -10,6 +12,7 @@ contract Contract {
 
     mapping(address => User) public users;
 
+    // Creates a new user account with a default balance of 100 tokens.
     function createUser() external {
 		require(users[msg.sender].isActive != true, "Account has already been created.");
         
@@ -17,6 +20,7 @@ contract Contract {
 		users[msg.sender].isActive = true;
     }
 
+    // Transfers tokens from the sender to another active user.
     function transfer(address _address, uint amount) external {
         require(users[msg.sender].isActive == true && users[_address].isActive == true, 'Both parties need to be active users.');
         require(users[msg.sender].balance >= amount, "Transfer amount exceeds balance.");
@@ -25,4 +29,3 @@ contract Contract {
         users[_address].balance += amount;
     }
 }
-
