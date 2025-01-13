329b79ecee682588aa65b4183da747bbe7de23e2
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-13 15:24:00 -0700

    added comments for each function in sending.sol

--- solidity/sending.sol
+++ solidity/sending.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+// A contract that supports receiving Ether, donating its balance to charity, and tipping the owner.
 contract Contract {
 	address public owner;
 	address public charity;
@@ -14,13 +15,13 @@ contract Contract {
 	// Fallback function to receive Ether.
 	receive() external payable { }
 
-	// Function to donate all contract balance to charity.
+	// Donates all contract balance to charity.
 	function donate() public {
 		(bool success, ) = charity.call{ value: address(this).balance }("");
 		require(success);
 	}
 
-	// Function to send a tip to the contract owner.
+	// Sends a tip to the contract owner.
 	function tip() public payable {
 		(bool success, ) = owner.call{ value: msg.value }("");
 		require(success);
