e9762c40ecabaaf440bf4a48e7bec6a9dd35a902
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-14 10:02:57 -0700

    added commets to the collectible script

--- solidity/inheritance/collectible.sol
+++ solidity/inheritance/collectible.sol
@@ -3,9 +3,11 @@ import "./baseContracts.sol";
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+// Contract representing a collectible item with pricing, ownership, and transfer features.
 contract Collectible is Ownable, Transferable {
 	uint public price;
 
+    // Function to set the price of the collectible, restricted to the owner.
 	function markPrice(uint _price) external onlyOwner {
 		price = _price;
 	}
