b68c0bbaa82ab7f4c5793448fc7416fc2b06d2c1
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-03-07 10:44:04 -0700

    created a calldata example

--- solidity/calldata/caller.sol
+++ solidity/calldata/caller.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.4;
 contract Sidekick {
     
     function sendAlert(address hero, uint enemies, bool armed) external {
-        
         (bool success, ) = hero.call(abi.encodeWithSignature("alert(uint256,bool)", enemies, armed));
-
         require(success);
     }
 
     function relay(address hero, bytes memory data) external {
         (bool success, ) = hero.call(data);
-
         require(success);
     }
 }
